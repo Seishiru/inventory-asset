03129c141cf06b49211f5b77394906ccc484a7cf
Seishiru <rapharaphael54@gmail.com>
2025-11-29 13:24:45 +0800

    comment unreadable import

--- components/ColumnOptionsMenu.tsx
+++ components/ColumnOptionsMenu.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Button } from './ui/button';
 import { Input } from './ui/input';
-import { Label } from './ui/label';
+// import { Label } from './ui/label';
 import { Separator } from './ui/separator';
 import { Settings, Plus, Trash2 } from 'lucide-react';
 import {
